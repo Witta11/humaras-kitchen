421c1a42771edac32f4e8df6fa17ab437986e7dd
Witta Glöckner <alina.glocke@gmail.com>
2021-02-03 18:14:09 +0100

    redesigned

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def home
-    @recipes = Recipe.all
+    @recipes = Recipe.first(3)
     @recipe = Recipe.last
     @comments = Recipe.last.comments
   end
