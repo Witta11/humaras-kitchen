6a47b4436c51e33e58303cf7b216613f5ea16d9f
Witta Glöckner <alina.glocke@gmail.com>
2021-02-04 13:33:26 +0100

    bug fixing

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,5 @@
 class Recipe < ApplicationRecord
   has_many :comments, dependent: :destroy
-  has_one_attached :photo
   include PgSearch::Model
   pg_search_scope :search_by_title_and_description,
     against: [:title, :description],
