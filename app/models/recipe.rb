b56e11a6644b65c643f5c3a56c02a7ef73bfae1d
Witta Glöckner <alina.glocke@gmail.com>
2021-02-04 12:28:53 +0100

    clueless

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,6 @@
 class Recipe < ApplicationRecord
-  has_one_attached :photo
   has_many :comments, dependent: :destroy
+  has_one_attached :photo
   include PgSearch::Model
   pg_search_scope :search_by_title_and_description,
     against: [:title, :description],
