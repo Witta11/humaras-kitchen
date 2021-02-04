303ae5a0e8fffe4fb5f3ee8465f38f7065503c03
Witta Glöckner <alina.glocke@gmail.com>
2021-02-04 13:29:40 +0100

    bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
+  get 'about', to: 'pages#about'
+  get 'blog', to: 'pages#blog'
   resources :recipes, only: [:show, :index] do
     resources :comments, only: [:new, :create]
   end
-  resources :users, only: [:new, :create, :destroy]
   resources :comments, only: [:destroy]
-  get 'about', to: 'pages#about'
-  get 'blog', to: 'pages#blog'
+  resources :users, only: [:new, :create, :destroy]
 end
