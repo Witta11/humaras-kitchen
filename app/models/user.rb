ce27ccf2eb78be55bfd92e8aea449a4dc351e894
Witta Glöckner <alina.glocke@gmail.com>
2021-02-10 15:01:58 +0100

    installing device

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   validates :name, presence: true
   validates :email, format: { with: /\A[^@\s]+@([^@.\s]+\.)+[^@.\s]+\z/ }, uniqueness: { case_sensitive: false }, length: { minimum: 4, maximum: 254 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users
   root to: 'pages#home'
   get 'about', to: 'pages#about'
   get 'blog', to: 'pages#blog'
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
     resources :comments, only: [:new, :create]
   end
   resources :comments, only: [:destroy]
-  resources :users, only: [:new, :create, :destroy]
+  # resources :users, only: [:new, :create, :destroy]
 end
